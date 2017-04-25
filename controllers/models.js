fc07f04e97284b51a43b2aaf14a2a503c96c40a8
sagrath23 <sagrath23@gmail.com>
2017-04-24 23:38:24 -0500

    Some more stuff added

--- controllers/models.js
+++ controllers/models.js
@@ -19,7 +19,8 @@ exports.sync = function(req, res, next) {
       currencyRatesDependencies = [],
       customerDependencies
       salesOrdersDependencies = [],
-      salesOrderDetailsDependencies = [];
+      salesOrderDetailsDependencies = [],
+      output = "";
 
   //get dates data
   currenciesAndDatesData.push(sourceDb.query("SELECT MIN(cr.currencyratedate) AS mindate, MAX(cr.currencyratedate) AS maxdate, CONCAT(EXTRACT(YEAR FROM cr.currencyratedate),EXTRACT(MONTH FROM cr.currencyratedate)) AS datename FROM Sales.CurrencyRate cr GROUP BY datename ORDER BY mindate ASC", { type: sourceDb.QueryTypes.SELECT }));
@@ -31,6 +32,8 @@ exports.sync = function(req, res, next) {
   Promise.all(currenciesAndDatesData).then(function(responses){
     console.log("Dates & Currencies data loaded");
     //when the results arrive, we pass it to the data wharehouse
+    console.log(" dates founded: "+responses[0].length+"<br>");
+    console.log(" currencies founded: "+responses[1].length+"<br>");
     currencyRatesDependencies.push(Models.DatesDimension.bulkCreate(helpers.transformDates(responses[0])).then(function(){ return Models.DatesDimension.findAll();}));
     //push DateDimension dependency on sales Orders Dependencies
     salesOrdersDependencies.push(currencyRatesDependencies[0]);
@@ -41,10 +44,12 @@ exports.sync = function(req, res, next) {
     Promise.all(currencyRatesDependencies).then(
       function(dwhResponses){
         console.log("Dates & currencies data transform and loaded into datawharehouse");
+
         //now, we load currency rates facts in the datawharehouse
         sourceDb.query("SELECT * FROM Sales.CurrencyRate", {type: sourceDb.QueryTypes.SELECT}).then(
           function(currencyRates) {
             //transfrom & load to DWH Dimension
+            console.log(" currency rates founded: "+currencyRates.length+"<br>");
             Models.CurrencyRatesFact.bulkCreate(helpers.transformCurrencyRates(currencyRates, dwhResponses[1], dwhResponses[0])).then(function(){ console.log("currency rates facts loaded.");});
           });
       });
@@ -54,6 +59,7 @@ exports.sync = function(req, res, next) {
   sourceDb.query("SELECT * FROM Sales.SalesReason", { type: sourceDb.QueryTypes.SELECT })
     .then(function(reasons) {
       //transfrom & load to DWH Dimension
+      console.log(" sales reasons founded: "+reasons.length+"<br>");
       Models.SaleReasonsDimension.bulkCreate(helpers.transformSalesReasons(reasons))
         .then(function() {
           console.log("Sales Reasons Dimension Uploaded");
@@ -63,6 +69,7 @@ exports.sync = function(req, res, next) {
   //extract ShipMethods data from sourceDb
   salesOrdersDependencies.push(sourceDb.query("SELECT * FROM Purchasing.ShipMethod", { type: sourceDb.QueryTypes.SELECT })
     .then(function(shipMethods) {
+      console.log("shipMethods founded: "+shipMethods.length+"<br>");
       //transfrom & load to DWH Dimension
       return Models.ShipMethodsDimension.bulkCreate(helpers.transformShipMethods(shipMethods));
     }));
@@ -70,6 +77,7 @@ exports.sync = function(req, res, next) {
   //extract product categories & subcategories data from sourceDb
   Promise.all([sourceDb.query("SELECT psc.ProductSubcategoryId, pc.ProductCategoryID, pc.Name AS category_name,psc.Name AS subcategory_name FROM Production.ProductCategory pc RIGHT JOIN Production.ProductSubcategory psc ON psc.ProductCategoryID = pc.ProductCategoryID", { type: sourceDb.QueryTypes.SELECT })
     .then(function(categories) {
+      console.log("Product categories founded: "+categories.length+"<br>");
       //transfrom & load to DWH Dimension
       return Models.ProductCategoriesDimension.bulkCreate(helpers.transformProductCategories(categories));
     })])
@@ -79,6 +87,7 @@ exports.sync = function(req, res, next) {
       //extract products data from sourceDb
       sourceDb.query("SELECT pr.ProductID, pr.Name, pr.MakeFlag, pr.FinishedGoodsFlag,pr.Color,pr.StandardCost,pr.ListPrice,COALESCE(pr.ProductSubcategoryID,-1) AS ProductSubcategoryID FROM Production.Product pr ", { type: sourceDb.QueryTypes.SELECT })
         .then(function(products) {
+          console.log("Products founded: "+products.length+"<br>");
           //transfrom & load to DWH Dimension
           salesOrderDetailsDependencies.push(Models.ProductsDimension.bulkCreate(helpers.transformProducts(products)));
         });
@@ -88,6 +97,7 @@ exports.sync = function(req, res, next) {
   sourceDb.query("SELECT so.SpecialOfferID, so.Description, so.DiscountPct, so.Type, so.Category, so.StartDate, so.EndDate, so.MinQty, so.MaxQty FROM Sales.SpecialOffer so", { type: sourceDb.QueryTypes.SELECT })
     .then(function(specialOffers) {
       //transfrom & load to DWH Dimension
+      console.log("Special offers founded: "+specialOffers.length+"<br>");
       salesOrderDetailsDependencies.push(Models.SpecialOffersDimension.bulkCreate(helpers.transformSpecialOffers(specialOffers)));
     });    
     
@@ -95,6 +105,7 @@ exports.sync = function(req, res, next) {
   //we took all records that StoreID is NULL
   salesOrdersDependencies.push(sourceDb.query("SELECT cus.CustomerID, per.Title, per.FirstName, per.MiddleName, per.LastName FROM Sales.Customer cus INNER JOIN Person.Person per ON per.BusinessEntityID = cus.PersonID WHERE cus.PersonID IS NOT NULL AND cus.StoreID IS NULL", { type: sourceDb.QueryTypes.SELECT })
     .then(function(customers) {
+      console.log("Customers founded: "+customers.length+"<br>");
       //transfrom & load to DWH Dimension
       return Models.CustomersDimension.bulkCreate(helpers.transformCustomers(customers));
     }));  
@@ -103,12 +114,14 @@ exports.sync = function(req, res, next) {
   salesOrdersDependencies.push(sourceDb.query("SELECT * FROM Sales.SalesTerritory", { type: sourceDb.QueryTypes.SELECT })
     .then(function(salesTerritories) {
       //transfrom & load to DWH Dimension
+      console.log("sales territories founded: "+salesTerritories.length+"<br>");
       return Models.SaleTerritoriesDimension.bulkCreate(helpers.transformSaleTerritories(salesTerritories));
     }));  
 
   //extract sales persons data from sourceDb
   salesOrdersDependencies.push(sourceDb.query("SELECT sp.BusinessEntityID, per.Title, per.FirstName, per.MiddleName, per.LastName, sp.SalesQuota, sp.Bonus, sp.CommissionPct, sp.SalesYTD, sp.SalesLastYear FROM Sales.SalesPerson sp INNER JOIN Person.Person per ON per.BusinessEntityID = sp.BusinessEntityID", { type: sourceDb.QueryTypes.SELECT })
     .then(function(salesPersons) {
+      console.log("sales persons founded: "+salesPersons.length+"<br>");
       //transfrom & load to DWH Dimension
       return Models.SalesPersonsDimension.bulkCreate(helpers.transformSalePersons(salesPersons));
     }));
@@ -119,8 +132,12 @@ exports.sync = function(req, res, next) {
     //extract Sales Orders from sourceDb  
     sourceDb.query("SELECT so.SalesOrderID, so.RevisionNumber, so.OrderDate, so.dueDate, so.ShipDate, so.Status, so.OnlineOrderFlag, so.PurchaseOrderNumber, so.AccountNumber, so.CustomerID, so.SalesPersonID, so.TerritoryID, so.ShipMethodID, so.TaxAmt, so.Freight, so.TotalDue, so.Comment FROM Sales.SalesOrderHeader so WHERE so.CustomerID IN (SELECT cus.CustomerID FROM Sales.Customer cus INNER JOIN Person.Person per ON per.BusinessEntityID = cus.PersonID WHERE cus.PersonID IS NOT NULL AND cus.StoreID IS NULL)", { type: sourceDb.QueryTypes.SELECT })
       .then(function(salesOrders) {
+        console.log("sales orders founded: "+salesOrders.length+"<br>");
         //transfrom & load to DWH Dimension
-        Models.SalesOrdersFact.bulkCreate(helpers.transformSalesOrders(salesOrders, responses[0]));
+        Promise.all([Models.SalesOrdersFact.bulkCreate(helpers.transformSalesOrders(salesOrders, responses[0])).then(function(){ return Models.SalesOrdersFact.findAll(); })]).then(function(response){
+          console.log("sales order added");
+          console.log(response[0]);
+        });
       });  
   });  
   //send response to view while we do all the stuff in background
