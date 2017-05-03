a9cc51803589960ba561c6ef17d6cdae03bdcfd5
sagrath23 <sagrath23@gmail.com>
2017-05-03 17:44:04 -0500

    Force sync set to false

--- models/bootstrap.js
+++ models/bootstrap.js
@@ -43,7 +43,7 @@ var CurrenciesDimension = require('./CurrenciesDimension').CurrenciesDimension;
 var GeographiesDimension = require('./GeographiesDimension').GeographiesDimension;
 //sync model
 db.sync({
-  force: true
+  force: false
 }).then(function() {
   console.log("Dimensional model created");
   return true;
