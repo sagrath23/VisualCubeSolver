83150ccde0896b8f6f735a0ee36395181ab02534
Steven Augusto Villegas <sagrath23@gmail.com>
2017-05-03 20:14:29 -0500

    testing....

--- controllers/reports.js
+++ controllers/reports.js
@@ -44,8 +44,7 @@ exports.getSalesCountPerMonth = function(req, res, next) {
             FROM 
               sales_orders_facts sof
               INNER JOIN  dates_dimensions dd ON dd."dateDimensionId" = sof."dateDimensionId"
-            GROUP BY dd."dateName"
-            ORDER BY dd."dateName" ASC`, { type: db.QueryTypes.SELECT })
+            GROUP BY dd."dateName"`, { type: db.QueryTypes.SELECT })
   .then(function(result){
     console.log("-----------------------------------------gotcha count!!!");
     console.log(result);
